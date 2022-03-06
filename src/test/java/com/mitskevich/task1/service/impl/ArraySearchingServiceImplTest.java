5d089b7c31f569cbcfc7a0ae5a06921d25b44b59
PavelMitskevich <paulmitskevich@gmail.com>
2022-03-06 21:20:22 +0300

    task1

--- src/test/java/com/mitskevich/task1/service/impl/ArraySearchingServiceImplTest.java
+++ src/test/java/com/mitskevich/task1/service/impl/ArraySearchingServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.mitskevich.task1.service.impl;
 
 import com.mitskevich.task1.entity.CustomArray;
+import com.mitskevich.task1.exception.CustomCommonException;
 import com.mitskevich.task1.service.ArraySearchingService;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -10,22 +11,32 @@ import static org.testng.Assert.*;
 public class ArraySearchingServiceImplTest {
     ArraySearchingService searchingService = new ArraySearchingServiceImpl();
 
-    @DataProvider(name = "dataForSearching")
-    public Object[][] dataForSearching() {
-        return new Object[][]{{new CustomArray(1, 3, 5, 7), 4},
-                {new CustomArray(-6, 12, 2, 8), 4},
-                {new CustomArray(1, -1, 25, 5), 7.5},
-                {new CustomArray(1035, 153, 56, 7, 12), 252.6},
-                {new CustomArray(1, 3), 2},
-                {new CustomArray(5, 0, 12), 9},
+    @DataProvider(name = "dataForSearchingMax")
+    public Object[][] dataForSearchingMax() {
+        return new Object[][]{{new CustomArray(1, 3, 5, 7), 7},
+                {new CustomArray(-6, 12, 2, 8), -6},
+                {new CustomArray(1, -1, 25, 5), 25},
+                {new CustomArray(1, 3, 5, 7), 7}};
+    }
+
+    @DataProvider(name = "dataForSearchingMin")
+    public Object[][] dataForSearchingMin() {
+        return new Object[][]{{new CustomArray(1, 3, 5, 7), 1},
+                {new CustomArray(-6, 12, 2, 8), -6},
+                {new CustomArray(1, -1, 25, 5), 1},
+                {new CustomArray(1035, 153, 56, 7, 12), 7},
                 {new CustomArray(1, 3, 5, 7), 4}};
     }
 
-    @Test
-    public void testSearchingMinimal() {
+    @Test(dataProvider = "dataForSearchingMin")
+    public void testSearchingMinimal(CustomArray array, int expected) throws CustomCommonException {
+        int actual = searchingService.searchingMinimal(array).getAsInt();
+        assertEquals(actual, expected);
     }
 
-    @Test
-    public void testSearchingMaximum() {
+    @Test(dataProvider = "dataForSearchingMax")
+    public void testSearchingMaximum(CustomArray array, int expected) throws CustomCommonException {
+        int actual = searchingService.searchingMaximum(array).getAsInt();
+        assertEquals(actual, expected);
     }
 }
